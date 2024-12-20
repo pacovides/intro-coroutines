f8b2e60f9cc014e8b28f9a4e10ee4a9f387665f4
Francisco Vides <pacovides@gmail.com>
2024-12-19 18:02:37 -0800

    Task 5

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,27 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    // My first attempt
+    //    val deferredUsers: MutableList<Deferred<List<User>>> = mutableListOf()
+    //    for (repo: Repo in repos) {
+    //        deferredUsers.add(async {
+    //            service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.body() ?: emptyList()
+    //        })
+    //    }
+
+    //Idiomatic
+    val deferredUsers: List<Deferred<List<User>>> = repos.map { repo ->
+        async(Dispatchers.Default) {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(req.org, repo.name)
+                .also { logUsers(repo, it) }
+                .body() ?: emptyList()
+        }
+    }
+    deferredUsers.awaitAll().flatten().aggregate()
 }
