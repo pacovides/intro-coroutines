6348a0593ecefbaa23a056cbf701d91e2a8dacff
Francisco Vides <pacovides@gmail.com>
2024-12-23 16:57:28 -0800

    Task 6 v1

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -5,7 +5,20 @@ import contributors.*
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
-    updateResults: suspend (List<User>, completed: Boolean) -> Unit
+    updateResults: suspend (List<User>, completed: Boolean) -> Unit,
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val users: MutableList<User> = mutableListOf()
+    for (repo: Repo in repos) {
+        val newUsers = service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.body() ?: emptyList()
+        users.addAll(newUsers)
+        updateResults(users.aggregate(), false)
+    }
+
+    //Could be avoided with a countdown in the loop above
+    updateResults(users.aggregate(), true)
 }
