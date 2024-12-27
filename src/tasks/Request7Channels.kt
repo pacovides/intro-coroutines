0d94c473880dd36978da5c88d1a48e4a13ae084a
Francisco Vides <pacovides@gmail.com>
2024-12-26 17:32:09 -0800

    Task 7

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -8,9 +8,26 @@ import kotlinx.coroutines.launch
 suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
-    updateResults: suspend (List<User>, completed: Boolean) -> Unit
+    updateResults: suspend (List<User>, completed: Boolean) -> Unit,
 ) {
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
     coroutineScope {
-        TODO()
+        val channel = Channel<List<User>>()
+        val users: MutableList<User> = mutableListOf()
+        for (repo: Repo in repos) {
+            launch {
+                val newUsers =
+                    service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.body() ?: emptyList()
+                channel.send(newUsers)
+            }
+        }
+        repeat(repos.size) {
+            users.addAll(channel.receive())
+            updateResults(users.aggregate(), it == repos.lastIndex)
+        }
     }
 }
